7312fe4af3fb13a8b8d0dae3906e5b7255bbb8a5
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-25 22:52:11 +0300

    002-StockContrroller added.

---
+++ FinanceWebSite.API/Controllers/StockController.cs
@@ -0,0 +1,38 @@
+﻿using FinanceWebSite.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceWebSite.API.Controllers
+{
+	[Route("api/stock")]
+	[ApiController]
+	public class StockController : ControllerBase
+	{
+        private readonly ApplicationDbContext _context;
+
+		public StockController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var stocks = _context.Stocks.ToList();
+			return Ok(stocks);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById([FromRoute] int id)
+		{
+			// if you have only id parameter Find() method is more eff than SingleOrDefault() Method
+			// because Find() method searched directly on Primary Key.
+			var stock = _context.Stocks.Find(id);
+
+			if (stock == null)
+				return NotFound();
+
+			return Ok(stock);
+		}
+	}
+}
