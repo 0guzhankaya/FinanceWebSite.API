c66293502e6021145325fdef66b238e6817af32e
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-26 20:31:39 +0300

    009-Pagination added.

--- FinanceWebSite.API/Helpers/QueryObject.cs
+++ FinanceWebSite.API/Helpers/QueryObject.cs
@@ -9,5 +9,9 @@
 		// Sorting
 		public string? SortBy { get; set; } = null;
 		public bool IsDecsending { get; set; } = false;
+
+		// Pagination
+		public int PageNumber { get; set; } = 1;
+		public int PageSize { get; set; } = 10;
 	}
 }
